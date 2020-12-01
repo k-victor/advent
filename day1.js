fa6b1c6b09e0faf9f894281c363b75c101b8c010
Victor Karlsson <victor.karlsson@qmatic.com>
2020-12-01 18:17:48 +0100

    day1

--- day1.js
+++ day1.js
@@ -1,8 +1,7 @@
 function productOfTermsInSumOf2020(numbers, numberOfTerms) {
   return numbers
-    .flatMap((expense, index) => getPermutations([expense], tailFromIndex(numbers, index + 1), numberOfTerms))
-    .map(permutation => sumOfList(permutation) === 2020 ? productOfList(permutation) : 0)
-    .find(product => product > 0);
+    .flatMap((number, index) => getPermutations([number], tailFromIndex(numbers, index + 1), numberOfTerms))
+    .reduce((previousValue, permutation) => sumOfList(permutation) === 2020 ? productOfList(permutation) : previousValue, null)
 }
 
 const getPermutations = (head, tail, numberOfTerms) =>
@@ -14,4 +13,5 @@ const tailFromIndex = (list, index) => list.slice(index, list.length);
 const sumOfList = (list) => list.reduce((a, b) => a + b);
 const productOfList = (list) => list.reduce((a, b) => a * b);
 
-// Eg. productOfTermsInSumOf2020([2000, 12312412, 412412521, 5215, 123123125, 215215, 1, 32232215, 12144225, 11244212, 4, 51244211, 12435, 10, 124213, 1124425, 5], 5) => 400000
+// Eg. productOfTermsInSumOf2020([2000, 12312412, 412412521, 5215, 123123125, 215215, 1, 32232215, 12144225, 11244212, 4, 51244211, 12435, 10, 124213, 1124425, 5], 5)
+//     => 400000
