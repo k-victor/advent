59d48b816dcf9874e6d1b23e684bed8022e6f6a8
Victor Karlsson <victor.karlsson@qmatic.com>
2020-12-01 18:18:48 +0100

    Day 1

--- day1.js
+++ day1.js
@@ -1,4 +1,4 @@
-function productOfTermsInSumOf2020(numbers, numberOfTerms) {
+function productOfTermsInSumOf2020(numberOfTerms, numbers) {
   return numbers
     .flatMap((number, index) => getPermutations([number], tailFromIndex(numbers, index + 1), numberOfTerms))
     .reduce((previousValue, permutation) => sumOfList(permutation) === 2020 ? productOfList(permutation) : previousValue, null)
@@ -13,5 +13,5 @@ const tailFromIndex = (list, index) => list.slice(index, list.length);
 const sumOfList = (list) => list.reduce((a, b) => a + b);
 const productOfList = (list) => list.reduce((a, b) => a * b);
 
-// Eg. productOfTermsInSumOf2020([2000, 12312412, 412412521, 5215, 123123125, 215215, 1, 32232215, 12144225, 11244212, 4, 51244211, 12435, 10, 124213, 1124425, 5], 5)
+// Eg. productOfTermsInSumOf2020(5, [2000, 12312412, 412412521, 5215, 123123125, 215215, 1, 32232215, 12144225, 11244212, 4, 51244211, 12435, 10, 124213, 1124425, 5])
 //     => 400000
